5af8627c36698a742caff2d67e038f8f99116692
Cameron Gross <camiam144@gmail.com>
2025-09-13 18:42:03 -0400

    Cleaned up models

--- src/main.rs
+++ src/main.rs
@@ -10,20 +10,20 @@ use db::{insert_all_rows, new_schema};
 use crate::db::get_most_recent_entry;
 
 fn main() -> Result<()> {
-    let path = Path::new("data/response_1757636788999.json");
-    let json_file = std::fs::File::open(path).unwrap();
-    let reader = std::io::BufReader::new(json_file);
+    let path: &Path = Path::new("data/response_1757636788999.json");
+    let json_file: std::fs::File = std::fs::File::open(path).unwrap();
+    let reader: std::io::BufReader<std::fs::File> = std::io::BufReader::new(json_file);
 
     let my_data: Vec<ExchangeRecord> = serde_json::from_reader(reader).unwrap();
     // println!("{:?}", my_data[0])
-    let db_path = Path::new("data/exchangedata.db");
-    let mut conn = Connection::open(db_path).expect("Couldn't open db");
+    let db_path: &Path = Path::new("data/exchangedata.db");
+    let mut conn: Connection = Connection::open(db_path).expect("Couldn't open db");
     new_schema(&conn).expect("Couldn't make schema");
 
     insert_all_rows(&my_data, &mut conn)?;
     conn.close().expect("Couldn't close db: ");
-    let mut conn2 = Connection::open(db_path).expect("Couldn't open db:");
-    let query_res = get_most_recent_entry(&mut conn2);
+    let mut conn2: Connection = Connection::open(db_path).expect("Couldn't open db:");
+    let query_res: Vec<models::ExchangeQueryResult> = get_most_recent_entry(&mut conn2);
     conn2.close().expect("Couldn't close connection: ");
     for elem in &query_res[..=2] {
         println!("{:?}", elem)
--- src/models.rs
+++ src/models.rs
@@ -1,42 +1,38 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize)]
+#[serde(rename_all = "PascalCase")]
 pub struct ExchangeRecord {
     #[serde(rename = "CurrencyExchangeSnapshotPairId")]
     pub pair_id: u64,
     #[serde(rename = "CurrencyExchangeSnapshotId")]
     pub snapshot_id: u64,
-    #[serde(rename = "Volume")]
     pub volume: String,
-    #[serde(rename = "CurrencyOne")]
     pub currency_one: CurrencyInfo,
-    #[serde(rename = "CurrencyTwo")]
     pub currency_two: CurrencyInfo,
-    #[serde(rename = "CurrencyOneData")]
     pub currency_one_data: CurrencyData,
-    #[serde(rename = "CurrencyTwoData")]
     pub currency_two_data: CurrencyData,
 }
 
 #[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct CurrencyInfo {
-    #[serde(rename = "id")]
     pub id: u64,
-    #[serde(rename = "itemId")]
     pub item_id: u64,
-    #[serde(rename = "apiId")]
+    pub currency_category_id: u64,
     pub api_id: String,
-    #[serde(rename = "text")]
     pub text: String,
+    pub category_api_id: String,
+    pub icon_url: String,
 }
 
 #[derive(Debug, Deserialize)]
+#[serde(rename_all = "PascalCase")]
 pub struct CurrencyData {
-    #[serde(rename = "RelativePrice")]
+    pub highest_stock: u64,
     pub relative_price: String,
-    #[serde(rename = "StockValue")]
     pub stock_value: String,
-    #[serde(rename = "VolumeTraded")]
+    pub value_traded: String,
     pub volume_traded: u64,
 }
 
@@ -50,4 +46,12 @@ pub struct ExchangeQueryResult {
     pub from_relative_price: f64,
     pub to_relative_price: f64,
     pub volume: f64,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+struct CurrencyExchangeSnapshot {
+    epoch: u64,
+    market_cap: String,
+    volume: String,
 }
